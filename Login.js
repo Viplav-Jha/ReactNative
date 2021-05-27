21cbb2aa83234bebc3f5bd1cfb07506cd5d41f3c
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-05-27 22:13:34 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,Text} from 'react-native';
+
+
+const Login = ()=>{
+    return(
+    <View>   
+           <Text>Hello !, This is login screen</Text>
+    </View>
+    )
+}
+
+export default Login;
