96c1c97a04d985370d1a1b9634b57925cf8357c7
Viplav Jha <67279519+Viplav-Jha@users.noreply.github.com>
2021-06-01 22:31:22 +0530

    Update From.js

--- From.js
+++ From.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, TextInput,Button,StyleSheet} from 'react-native'
+import {View, TextInput,Button,StyleSheet} from 'react-native';
 
 class Forms extends React.Component {
     constructor() {
